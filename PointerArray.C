38b61a2ae58398e37659bdd9f15085a4cc5e8f5f
harshitha-pai <harshithapaiofficial03@gmail.com>
2025-02-12 12:05:43 +0530

    few topics

---
+++ PointerArray.C
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int arr[6]={1,2,3,4,5,6};
+    int *ptr=arr;
+
+    printf("Value at the pointer %d\n",*ptr);
+}
---
+++ ReverseNumber.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    int rev=0;
+
+    while (num != 0) {
+      int rem = num % 10;      
+        rev=rev*10+rem;
+        num = num / 10;         
+    }
+
+        printf("%d",rev);
+
+
+    return 0;
+}
