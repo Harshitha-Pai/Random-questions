364a4a90c09f057b90d1476774eb3e42fb7d2314
Harshitha A Pai <137300653+Harshitha-Pai@users.noreply.github.com>
2025-02-13 14:55:05 +0530

    Update PointerArray.C

--- PointerArray.C
+++ PointerArray.C
@@ -4,5 +4,8 @@ int main(){
     int arr[6]={1,2,3,4,5,6};
     int *ptr=arr;
 
-    printf("Value at the pointer %d\n",*ptr);
+    printf("Array elements:");
+    for(int i=0;i<6;i++){
+        printf("%d\t",*(ptr+i));
+    }
 }
